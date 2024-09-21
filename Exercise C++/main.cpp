0d91c1bd8584f4f4200e141fbdfb3e8a6af31a46
Muhammad Ayom Izzuddin <ayom.izzudin@gtech.digital>
2024-09-21 20:35:43 +0700

    init commit hitung keliling persegi panjang

---
+++ Exercise C++/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    /* Hitung keliling persegi panjang */
+
+    float panjang = 18;
+    float lebar = 7;
+    float keliling;
+
+    keliling = 2 * (panjang + lebar);
+
+    cout << "Panjang: " << panjang << endl;
+    cout << "Lebar: " << lebar << endl;
+    cout << "Keliling persegi panjang adalah: " << keliling << endl;
+}
